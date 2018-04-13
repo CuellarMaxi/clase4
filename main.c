490e3d3953a715689cfd8ca7c3aafddc1170c4e6
CuellarMaxi <maxi_boca_la3@hotmail.com>
2018-04-13 14:54:52 -0300

    se cambio el 5 por 7

--- main.c
+++ main.c
@@ -6,13 +6,13 @@ int main()
     int numero;
     printf("ingrese numero \n");
     scanf("%d",&numero);
-        if (numero>5)
+        if (numero>7)
         {
-            printf("numero mayor que 5 \nEl numero es: %d",numero);
+            printf("numero mayor que 7 \nEl numero es: %d",numero);
         }
         else
         {
-            printf("numero menor que 5 \nEl numero es: %d",numero);
+            printf("numero menor que 7 \nEl numero es: %d",numero);
         }
     return 0;
 }
